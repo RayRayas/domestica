5a60b24a2d40e7e2a963eae296b50b7a110c3f39
Ray Rayas <77710071+RayRayas@users.noreply.github.com>
2021-06-22 21:40:53 -0500

    empezamos la logia de progrmacion de funciones y condiciones

--- src/formulario.js
+++ src/formulario.js
@@ -7,3 +7,18 @@ function numeroDeCuartos() {
     alert(numerohab);   
 }
 
+var checkbox = document.getElementById('checkbox1');
+checkbox.addEventListener("change", validaCheckbox, false);
+ function validaCheckbox()
+{
+ 
+  var checked = checkbox.checked;
+  if(checked){
+    document.getElementById('hora').innerHTML =  "<option selected>Horario</option><option >8:30 am</option><option >9:30 am</option><option >10:30 am</option><option >11:30 am</option><option >12:30 pm</option><option >1:30 pm</option><option >2:30 pm</option><option >3:30 pm</option><option >4:30 pm</option><option>5:30 pm</option>";
+                   
+  }
+  else 
+    document.getElementById('hora').innerHTML = "<option selected>Horario</option><option >8:00 am</option><option >9:00 am</option><option >10:00 am</option><option >11:00 am</option><option >12:00 pm</option><option >1:00 pm</option><option >2:00 pm</option><option >3:00 pm</option><option >4:00 pm</option><option>5:00 pm</option>";
+
+  
+};
--- src/route/index.js
+++ src/route/index.js
@@ -1,12 +1,29 @@
 const {Router} = require('express');
 const router = Router();
 
+
+
 router.post('/email', (req,res) => {
-    const {name, email, phone, message} = req.body;
+    const {nombre, mail, telefono, ciudad, direccion, colonia, tipo, cuartos, banos, fecha, hora, mascotas, limpieza, Lavado, planchado, cocina} = req.body;
+    
     contentHTMML = `
     
     <h1> informacion </h1>
-    <p>popo: ${name} </p>`
+    <p>nombre: ${nombre} </p>
+    <p>mail: ${mail} </p>
+    <p>telefono: ${telefono} </p>
+    <p>ciudad: ${ciudad} </p>
+    <p>Dirección: ${direccion, colonia} </p>
+    <p>Tipo: ${tipo} </p>
+    <p>Cuartos: ${cuartos} </p>
+    <p>Baños: ${banos} </p>
+    <p>Fecha: ${fecha} </p>
+    <p>Hora: ${hora} </p>
+    <p>Mascotas: ${mascotas} </p>
+    <p>Limpieza: ${limpieza} </p>
+    <p>Lavado: ${Lavado} </p>
+    <p>Planchado: ${planchado} </p>
+    <p>Cocina: ${cocina} </p>`
     res.send('received');
     console.log(contentHTMML);
 });
