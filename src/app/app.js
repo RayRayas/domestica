15c751c10c85348d3023489bf5dde0fdc8e56b2b
Ray Rayas <77710071+RayRayas@users.noreply.github.com>
2021-06-28 00:23:22 -0500

    iniciamos nodemailer en correo personal, en espera de los coreos reales y modificar la fecha con 0

---
+++ src/app/app.js
@@ -0,0 +1,71 @@
+
+
+
+// Cambio de valor en range cuartos
+function rangeSlide(value) {
+    document.getElementById('cuartos').innerHTML = value;
+}
+
+// Cambio de valor en range baños
+function rangeBath(value) {
+    document.getElementById('banos').innerHTML = value;
+}
+
+
+// Parametros de fecha y cambio de precio
+var fechado = new Date(); 
+var mes = fechado.getMonth()+1; 
+var dia = fechado.getDate(); 
+var ano = fechado.getFullYear(); 
+var ano2= ano + 1;
+var dia2 = dia + 1;
+var mes0 = "0" + mes;
+var tomorrow = fechado.getDate()+1;
+  
+
+        //    Fijamos fechas 
+window.onload = function (){
+     if (mes < 10)
+        mes = '0' + mes;
+     if(dia<10)
+        dia='0'+dia;                        
+                        document.getElementById('fecha').min=ano+"-"+mes+"-"+dia;
+                        document.getElementById('fecha').max=ano2+"-01-31";
+                  return mes;
+                  return dia;
+                      }
+                      
+let mesesA = mes;
+var servExp = document.getElementById("fecha");
+servExp.addEventListener("change", validaExp, false);
+let fechaC = ano+"-"+mes0+"-"+dia;
+var manana = ano+"-"+mes0+"-"+tomorrow;
+
+        //   Validamos el dia si el servicio es hoy o mañana
+function validaExp () {                    
+     if (servExp.value == fechaC) {
+           document.getElementById('express').innerHTML = "<option >4hrs $430</option><option >5hrs $525</option><option >6hrs $615</option><option >7hrs $695</option><option >8hrs $785</option>";
+          } 
+     else if (servExp.value ==  manana )
+           document.getElementById('express').innerHTML = "<option >4hrs $430</option><option >5hrs $525</option><option >6hrs $615</option><option >7hrs $695</option><option >8hrs $785</option>";
+     else 
+           document.getElementById("express").innerHTML = "<option >4hrs $390</option><option >5hrs $475</option><option >6hrs $540</option><option >7hrs $630</option><option >8hrs $720</option>";
+ };
+
+
+//  Cambio de hora
+ var checkbox = document.getElementById('checkbox1');
+checkbox.addEventListener("change", validaCheckbox, false);
+ function validaCheckbox()
+{
+ 
+  var checked = checkbox.checked;
+  if(checked){
+    document.getElementById('hora').innerHTML =  "<option selected>Horario</option><option >8:30 am</option><option >9:30 am</option><option >10:30 am</option><option >11:30 am</option><option >12:30 pm</option><option >1:30 pm</option><option >2:30 pm</option><option >3:30 pm</option><option >4:30 pm</option><option>5:30 pm</option>";
+                   
+  }
+  else 
+    document.getElementById('hora').innerHTML = "<option selected>Horario</option><option >8:00 am</option><option >9:00 am</option><option >10:00 am</option><option >11:00 am</option><option >12:00 pm</option><option >1:00 pm</option><option >2:00 pm</option><option >3:00 pm</option><option >4:00 pm</option><option>5:00 pm</option>";
+
+  
+}
--- src/route/index.js
+++ src/route/index.js
@@ -1,32 +1,68 @@
 const {Router} = require('express');
+const nodemailer = require('nodemailer');
 const router = Router();
 
 
 
-router.post('/email', (req,res) => {
-    const {nombre, mail, telefono, ciudad, direccion, colonia, tipo, cuartos, banos, fecha, hora, mascotas, limpieza, Lavado, planchado, cocina} = req.body;
+router.post('/email', async(req,res) => {
+    const {nombre, mail, telefono, ciudad, direccion, colonia, tipo, cuartos, banos, fecha, hora, mascotas, limpieza, Lavado, planchado, cocina, entrada, horas} = req.body;
     
     contentHTMML = `
+
+    <h1> Domestica Tu Aliada </h1>
+    <h2> Nueva Cita Generada </h2>
+    <h3>Esta es la informacion ingresada por el cliente</h3>
+    <ul>
+    <li>nombre: ${nombre} </li>
+    <li>mail: ${mail} </li>
+    <li>telefono: ${telefono} </li>
+    <li>ciudad: ${ciudad} </li>
+    <li>Dirección: ${direccion, colonia} </li>
+    <li>Tipo: ${tipo} </li>
+    <li>Cuartos: ${cuartos} </li>
+    <li>Baños: ${banos} </li>
+    <li>Fecha: ${fecha} </li>
+    <li>Hora: ${hora} </li>
+    <li>Cuantas horas: ${horas}</li>
+    <li>¿Como entrara?: ${entrada} </li>
+    <li>Mascotas: ${mascotas} </li>
+    <li>Limliieza: ${limpieza} </li>
+    <li>Lavado: ${Lavado} </li>
+    <li>Planchado: ${planchado} </li>
+    <li>Cocina: ${cocina} </li>
+    </ul>
+    `
+   
+
     
-    <h1> informacion </h1>
-    <p>nombre: ${nombre} </p>
-    <p>mail: ${mail} </p>
-    <p>telefono: ${telefono} </p>
-    <p>ciudad: ${ciudad} </p>
-    <p>Dirección: ${direccion, colonia} </p>
-    <p>Tipo: ${tipo} </p>
-    <p>Cuartos: ${cuartos} </p>
-    <p>Baños: ${banos} </p>
-    <p>Fecha: ${fecha} </p>
-    <p>Hora: ${hora} </p>
-    <p>Mascotas: ${mascotas} </p>
-    <p>Limpieza: ${limpieza} </p>
-    <p>Lavado: ${Lavado} </p>
-    <p>Planchado: ${planchado} </p>
-    <p>Cocina: ${cocina} </p>`
-    res.send('received');
+
+    const transporter = nodemailer.createTransport({
+        host: 'mail.domesticatualiada.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'webmaster@domesticatualiada.com',
+            pass: 'I[h?htHKThP&'
+        },
+        tls: {
+            rejectUnauthorized: false,
+
+        },
+    });
+
+    const info = await transporter.sendMail ({
+        from: "'Domestica tu aliada' <webmaster@domesticatualiada.com>",
+        to: 'rayas.duran@gmail.com', 
+        subject: 'Nueva Cita',
+        html: contentHTMML,
+
+    });
+    console.log('mensaje enviado', info.messageId)
+
+    res.redirect('/gracias.html');
     console.log(contentHTMML);
+
 });
 
-module.exports = router; 
 
+module.exports = router;
